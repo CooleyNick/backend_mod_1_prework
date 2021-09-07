76247c1455eda6089f0a723970dbd07ce1ab7dd8
Nick Cooley <cooley8706@gmail.com>
2021-09-06 18:17:18 -0600

    Add Section 1

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,59 +1,71 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
 # prints what that variable holds to the terminal:
-name = "Harry Potter"
+name = "Nick Cooley"
 p name
 
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+message_potter = "Harry Potter must not return to Hogwarts"
+p message_potter
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
 # YOUR CODE HERE
-p students
+p students + 2
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
 # YOUR CODE HERE
-p students
+p students - 2
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
+first_name = "Nick"
+is_hungry = true
+number_of_pets = 1
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
-# Explain.
+# first name was a string so I utilized "" to assign properly. It does not have a stored value or any numerical purpose.
+# is_hungry was a boolean value thus was set to true. Let's be honest, it's always true with me.
+# number_of_pets was an integer value so I delcared it as such.
 
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
-
+first_name = "Nicholas";
+is_hungry = false; # I hate lying about things like this.
+number_of_pets = 12;
+p first_name
+p is_hungry
+p number_of_pets
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
+p healthy_snacks + junk_food_snacks
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
--- section1/reflection.md
+++ section1/reflection.md
@@ -1,19 +1,23 @@
 ## Section 1 Reflection
 
 1. How did the SuperLearner Article resonate with you? What from this list do you already do? Want to start doing or do more of? Is there anything not on this list, that you would add to it?
+Reading and constantly practicing is something I want to strive to reach on a daily basis. I am excited that I can practice coding as much as I have time for. The approach of 10,000 hours is the ultimate goal with quality practice.
 
 1. How would you print the string `"Hello World!"` to the terminal?
+p "Hello World" or I would utilize the IRB and use the greeting = "Hello World"
 
 1. What character is used to indicate comments in a ruby file?
-
+#
 1. Explain the difference between an integer and a float?
-
+Integer is a whole number whereas a float uses a decimal with the latter being more accurate in equations.
 1. In the space below, create a variable `animal` that holds the string `"zebra"`
-
+animal = "zebra"
 1. How would you print the string `"zebra"` using the variable that you created above?
-
+p animal
 1. What is interpolation? Use interpolation to print a sentence using the variable `animal`.
-
+In computer programming, string interpolation (or variable interpolation, variable substitution, or variable expansion) is the process of evaluating a string literal containing one or more placeholders, yielding a result in which the placeholders are replaced with their corresponding values.
+#{animal} would print out zebra
 1. What method is used to get input from a user?
-
+gets.chomp
 1. Name and describe two common string methods:
+declaring a variable with "" or using interpolation #{  } with variables and imbedding strings within strings
