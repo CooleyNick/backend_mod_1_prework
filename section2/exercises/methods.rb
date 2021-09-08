207f515acac9d7fbe55fbb1c7905d9d82240f6d7
Nick Cooley <cooley8706@gmail.com>
2021-09-08 16:27:12 -0600

    Add section 2 work

--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -5,26 +5,40 @@
 
 # Example: Write a method that when called will print your name:
 def print_name
-  p "Severus Snape"
+  p "Nick Cooley"
 end
 
 print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method three times with different arguments passed in:
-# YOUR CODE HERE
+def sum(a,b)
+  sum = a + b;
+end
+p sum(10, 8)
+p sum(8, 9)
+p sum(95, 85)
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
-# Then call your method three times with different arguments passed in. 
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
+# Then call your method three times with different arguments passed in.
+def two_strings(a, b)
+  two_strings = ("#{a} and #{b}")
+end
+
+p two_strings("peanut butter", "jelly")
+p two_strings("lamb", "tuna fish")
+p two_strings("beer", "wings")
+
 
 
 #-------------------
@@ -32,10 +46,12 @@ print_name("Albus Dumbledore")
 #-------------------
 
 
-# Naming is notoriously hard in programming. It is a skill to name a variable or function concisely enough that it is reasonable to type, but descriptive enough that others can infer the meaning.
+# Naming is notoriously hard in programming. It is a skill to name a variable or function concisely enough
+# that it is reasonable to type, but descriptive enough that others can infer the meaning.
 
 # Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 # What did you name each parameter, and why?
 
-# EXPLAIN:
-
+# EXPLAIN: I named the function "two_strings" simply because of the requirement of the task.
+#Anybody reading this in the context of the lesson would have zero guesswork as to what I'm doing.
+#I named paramters according to food items that "go well together" and are commonly accepted
--- section2/reflection.md
+++ section2/reflection.md
@@ -1,29 +1,44 @@
 ## Section 2 Reflection
 
-1. Regarding the blog posts in Part A, how do you feel about asking questions? Do you tend to ask them too soon, or wait too long, or somewhere in between?
+1. Regarding the blog posts in Part A, how do you feel about asking questions? Do you tend to ask them too soon, or wait too long, or somewhere in between? I absolutely wait too long to ask questions. I attempt to answer
+questions on my own too often when I could've simply created a discussion regarding that topic that might've enhanced my knowledge even further. When talking with experts, they know where people stumble and are able to help avoid needless pitfalls in thought process.
 
 ### If Statements
 
 1. What is a conditional statement? Give three examples.
-
+Conditional statements are used to determine whether a condition is true or false based on predetermined data.
 1. Why might you want to use an if-statement?
-
+To determine whether or not multiple criteria determine a condition to be true or not.
 1. What is the Ruby syntax for an if statement?
-
+if #condition true
+puts
+end
 1. How do you add multiple conditions to an if statement?
-
+elseif
 1. Provide an example of the Ruby syntax for an if/elsif/else statement:
-
+if trails_dry = true
+  puts "let's ride"
+elseif trails_dry = false
+  puts "gym time"
+else
+  puts "call bike shop and ask"
+end
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
-
+You can utilize !, &&, and ||
 ### Methods
 
 1. In your own words, what is the purpose of a method?
-
+The purpose of a method is to eliminate the need to add certain bits of code repeatedly, to create a situation where one call can invoke several instances of a desired result.
 1. Create a method named `hello` that will print `"Sam I am"`.
-
+def hello
+puts "Sam I am"
+end
 1. Create a method named `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
-
+def hello_someone(words)
+puts words
+end
+hello_someone("Sam I am")
 1. How would you call or execute the method that you created above?
-
+puts words
 1. What questions do you have about methods in Ruby?
+Honestly this lesson is hard to follow despite rereading it over. How are methods utilized in real world applications?
